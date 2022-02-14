895e7ec83efc3904ba76ee935d33e8eb43e3c88e
myk4040okothogodo <mikeogodo5@gmail.com>
2022-02-14 14:44:52 +0300

    restpost

--- data/products.go
+++ data/products.go
@@ -3,6 +3,7 @@ package data
 import (
   "time"
   "io"
+  "fmt"
   "encoding/json"
 )
 
@@ -18,6 +19,14 @@ type Product struct {
   UpdatedOn  string      `json:"-"`
   DeletedOn  string      `json:"-"`
 }
+
+
+func (p *Product) FromJSON(r io.Reader) error{
+   e := json.NewDecoder(r)
+   return e.Decode(p)
+
+}
+
 
 //Products is a collection of Product
 type Products []*Product
@@ -36,6 +45,42 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func GetProducts() Products {
   return productList
+}
+
+func AddProduct(p *Product){
+  p.ID  = getNextID()
+  productList = append(productList, p)
+
+}
+
+
+func UpdateProduct(id int, p *Product) error {
+  _,pos, err  := findProduct(id)
+  if err != nil {
+    return err
+
+  }
+  p.ID = id
+  productList[pos] = p
+  return nil
+}
+
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product,int, error){
+  for i, p := range productList{
+    if p.ID == id {
+      return p ,i ,nil
+
+    }
+  }
+  return nil,-1,ErrProductNotFound 
+}
+
+func getNextID() int {
+  lp := productList[len(productList) - 1]
+  return lp.ID + 1
 }
 
 var productList = []*Product{
