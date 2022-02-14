04f479c9cb8f3d74457e53f5c87da9686ea33f78
myk4040okothogodo <mikeogodo5@gmail.com>
2022-02-14 19:14:29 +0300

    validation

--- go.mod
+++ go.mod
@@ -9,6 +9,12 @@ require (
 	github.com/myk4040okothogodo/GoMicroserve/handlers v0.0.0-00010101000000-000000000000
 )
 
-require github.com/myk4040okothogodo/GoMicroserve/data v0.0.0-00010101000000-000000000000 // indirect
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/myk4040okothogodo/GoMicroserve/data v0.0.0-00010101000000-000000000000 // indirect
+)
 
 replace github.com/myk4040okothogodo/GoMicroserve/data => ./data
--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
     "context"
+    "fmt"
     "log"
     "strconv"
     "github.com/gorilla/mux"
@@ -75,8 +76,16 @@ func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
 			return
 		}
-
-		// add the product to the context
+    // Validate the product
+    err = prod.Validate()
+    if err != nil {
+      p.l.Println("[Error] validating product",err)
+      http.Error(rw, fmt.Sprintf("Error validating product: %s", err), 
+      http.StatusBadRequest,
+    )
+      return
+    }  
+    // add the product to the context
 		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
 		r = r.WithContext(ctx)
 
